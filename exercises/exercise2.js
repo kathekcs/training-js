f0d7a573a97e52d8f867351661a6923fb5925bd9
katherine.cardonasalazar.contractor.gdevtools.com <katherine.cardonasalazar.contractor@gdevtools.com>
2022-12-16 11:25:50 -0500

    Fixed the comments

--- exercises/exercise2.js
+++ exercises/exercise2.js
@@ -3,9 +3,9 @@ const role = ['CEO', 'CTO', 'DEV', 'DEV'];
 const seniority = [5, 10, 4, 2];
 
 // Escribir un programa que imprima la información completa por empleado
-for(let i=0; i<employee.length; i++){
-    console.log(employee[i] + ' es el/la ' + role[i] + ' y tiene ' + seniority[i] + ' años de experiencia');
-};
+employee.forEach((item, index) => {
+    console.log(item + ' es el/la ' + role[index] + ' y tiene ' + seniority[index] + ' años de experiencia');
+});
 
 // Escribir un programa el nombre y el cargo el empleado con mas experiencia
 const seniorityMaxPosition = seniority.indexOf(Math.max(...seniority));
@@ -16,6 +16,6 @@ employee.push('Juanita','Sebastian','Santiago');
 role.push('DEV','Tech Lead','DEV');
 seniority.push(2, 3, 0);
 
-for(let i=4; i<employee.length; i++){
-    console.log(employee[i] + ' --> ' + role[i] + ' --> ' + seniority[i] + ' años');
-};
+employee.forEach((item, index) => {
+    console.log(item + ' --> ' + role[index] + ' --> ' + seniority[index] + ' años');
+});
